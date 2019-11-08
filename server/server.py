16fa78d33a807478f2e72692973f85b31b8de1ed
rpbranco <rafael.p.branco@tecnico.ulisboa.pt>
2019-11-08 18:29:05 +0000

    Add: connection to db

--- server/server.py
+++ server/server.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
 
 from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello'
+    return '<h1>Hello</h1>'
+
 
 if __name__ == '__main__':
-    app.run()
+    with sqlite3.connect('gym.db') as connection:
+        connection.execute('DROP TABLE IF EXISTS users')
+        connection.execute('CREATE TABLE users (id TEXT, nome TEXT, email TEXT, litros REAL)')
+        connection.execute('INSERT INTO users VALUES ("1", "Nuno", "nuno@gmail.com", 0)')
+        connection.execute('INSERT INTO users VALUES ("2", "Rui", "rui@gmail.com", 0)')
+        connection.commit()
+
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM users')
+        print(cursor.fetchall())
+
+#    app.run()
