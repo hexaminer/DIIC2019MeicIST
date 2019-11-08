a9ee8c0864614d944cf94efc7e64d897668ca3d3
rpbranco <rafael.p.branco@tecnico.ulisboa.pt>
2019-11-08 18:52:00 +0000

    Add: GET /users

--- server/server.py
+++ server/server.py
@@ -5,21 +5,26 @@ import sqlite3
 
 app = Flask(__name__)
 
+# Initialize DB
+connection = sqlite3.connect('gym.db');
+connection.execute('DROP TABLE IF EXISTS users')
+connection.execute('CREATE TABLE users (id TEXT PRIMARY KEY, nome TEXT, email TEXT, litros REAL)')
+for i in range(200):
+    connection.execute(f'INSERT INTO users VALUES ("{str(i)}", "Nuno", "nuno@gmail.com", 0)')
+connection.commit()
+
 @app.route('/')
 def hello():
     return '<h1>Hello</h1>'
 
+@app.route('/users', methods = ['GET'])
+def list_users():
+    connection = sqlite3.connect('gym.db');
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM users')
+    
+    return str(cursor.fetchall())
+
 
 if __name__ == '__main__':
-    with sqlite3.connect('gym.db') as connection:
-        connection.execute('DROP TABLE IF EXISTS users')
-        connection.execute('CREATE TABLE users (id TEXT, nome TEXT, email TEXT, litros REAL)')
-        connection.execute('INSERT INTO users VALUES ("1", "Nuno", "nuno@gmail.com", 0)')
-        connection.execute('INSERT INTO users VALUES ("2", "Rui", "rui@gmail.com", 0)')
-        connection.commit()
-
-        cursor = connection.cursor()
-        cursor.execute('SELECT * FROM users')
-        print(cursor.fetchall())
-
-#    app.run()
+    app.run()
